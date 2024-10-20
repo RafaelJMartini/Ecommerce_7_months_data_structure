cb65de0c69ce890af3d7057727573e908cee9af9
RafaelJMartini <143219127+RafaelJMartini@users.noreply.github.com>
2024-10-20 19:50:27 -0300

    Add files via upload
    Usado para trocar ",," por ", ,"

---
+++ separaVirgula.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+
+void substituirVirgulasDuplas(char *linha) {
+    char linha_modificada[1024];
+    int i = 0, j = 0;
+
+    while (linha[i] != '\0') {
+
+        if (linha[i] == ',' && linha[i+1] == ',') {
+            linha_modificada[j++] = ',';
+            linha_modificada[j++] = ' ';
+        } else {
+            linha_modificada[j++] = linha[i];
+        }
+        i++;
+    }
+    linha_modificada[j] = '\0';
+
+    strcpy(linha, linha_modificada);
+}
+
+int main() {
+    FILE *arqOriginal = fopen("2019-Nov.csv", "r");
+    FILE *arqNovo = fopen("arquivo_modificado.csv", "w");
+
+    if (arqOriginal == NULL || arqNovo == NULL) {
+        printf("Erro ao abrir os arquivos.\n");
+        return 1;
+    }
+
+
+    char linha[1024];
+
+
+    while (fgets(linha, sizeof(linha), arqOriginal) != NULL) {
+        substituirVirgulasDuplas(linha);
+        fputs(linha, arqNovo);
+    }
+
+    fclose(arqOriginal);
+    fclose(arqNovo);
+
+    printf("Arquivo processado e salvo como 'arquivo_modificado.csv'.\n");
+
+    return 0;
+}
